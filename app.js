a4b9fa5d518701a134acb6e012d4b824abdb472b
Sanusi Muhyideen <just4deen@yahoo.com>
2023-11-09 11:05:05 +0100

    first comit

---
+++ app.js
@@ -0,0 +1,23 @@
+// let test;
+// test = 10;
+
+// console.log(test);
+
+// const num = 2;
+// num = 6;
+// console.log(num);
+// const num = 2;
+// num = 6;
+// console.log(num);
+
+// const num;
+// num = 6;
+// console.log(num);
+
+// let test;
+
+// console.log(test);
+
+const num = 2;
+num = 6;
+console.log(num);
