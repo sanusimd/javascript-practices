814717a372a7a430335b88e0787ffb5adcb913e8
Sanusi Muhyideen <just4deen@yahoo.com>
2023-11-09 23:35:52 +0100

    practise practise

--- app.js
+++ app.js
@@ -18,6 +18,18 @@
 
 // console.log(test);
 
-const num = 2;
-num = 6;
-console.log(num);
+// const num = 2;
+// num = 6;
+// console.log(num);
+
+// console.log(Boolean(0));
+// const name = "james";
+// console.log(Boolean(name));
+const price = 10;
+console.log(Boolean(price));
+// let isTrue = true;
+// console.log(isTrue);
+
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean(-1));
